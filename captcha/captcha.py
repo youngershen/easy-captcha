823c811a851fe47d7cb4591092db16880b886df9
Younger Shen <younger.shen@hotmail.com>
2018-08-14 11:09:00 +0800

    init

--- captcha/captcha.py
+++ captcha/captcha.py
@@ -5,45 +5,53 @@
 # CELL : 13811754531
 # WECHAT : 13811754531
 # WEBSITE : www.punkcoder.cn
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Captcha:
-    font = None
-    font_size = 30
-    width = 60
-    height = 30
-    border = 1
-    text = 'hello world'
-    confuse_level = 0
-    confuse_type = 'simple'
+    FONT_DEFAULT = ''
 
-    def __init__(self):
+    FILE_FORMAT_JPG = 'jpg'
+    FILE_FORMAT_PNG = 'png'
+    FILE_FORMAT_GIF = 'gif'
+
+    STYLE_SIMPLE = 'simple'
+
+    IMAGE_MODE = 'L'
+
+    def get_file(self, text, path=None):
+        image = self._get_image(text)
+        image.save(path, self.file_format)
+        return image
+
+    def get_object(self, text):
+        return self._get_image(text)
+
+    def __init__(self, size, font_size, font='', file_format='png', style='simple'):
+        self.size = size
+        self.font_size = font_size
+        self.font = font
+        self.file_format = file_format
+        self.style = style
+
+    def _get_image(self, text):
+        image = Image.new(self.IMAGE_MODE, self.size)
+        self._fill_image(image)
+        self._text_image(text, image)
+        return image
+
+    def _text_image(self, text, image):
+        font = ImageFont.truetype(self.font, self.font_size)
+        d = ImageDraw.Draw(image)
+        d.text((0, 0), text, font=font, fill=())
+
+    def _fill_image(self, image):
         pass
-
-    def get_image(self, text):
-        self.text = text
-        captcha = self._get_captcha()
-        return captcha
-
-    def get_file(self, text, path):
-        self.text = text
-        captcha = self._get_captcha()
-        captcha.save(path)
 
     @property
-    def image_size(self):
-        return self.width, self.height
-
-    def _confuse(self):
+    def _xy(self):
         pass
 
-    def _confuse_simple(self):
+    @property
+    def _color(self):
         pass
-
-    def _get_captcha(self):
-        image = Image.new(mode="RGB", size=self.image_size, color=(255, 255, 255))
-        draw = ImageDraw.Draw(image)
-        # draw.text((self.border, self.border), self.text, font=self.font, fill="#000")
-        draw.text((self.border, self.border), self.text, fill="#000")
-        return image
--- captcha/tests.py
+++ captcha/tests.py
@@ -5,3 +5,13 @@
 # CELL : 13811754531
 # WECHAT : 13811754531
 # WEBSITE : www.punkcoder.cn
+from unittest import TestCase
+from .captcha import Captcha
+
+
+class CaptchaTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_alphabet(self):
+        captcha = Captcha(font=['a'], style=Captcha.STYLE_SIMPLE, format=Captcha.FORMAT_PNG)
